158dbefa22091becbdbe2f8b4c4b9b87fbc29dda
Matt Holland <mtholland10@gmail.com>
2017-01-23 16:34:03 -0600

    Update contact placeholder, update pdf copy of resume

--- controllers/documentController.js
+++ controllers/documentController.js
@@ -7,9 +7,10 @@ module.exports = {
     downloadResume: (req, res) => {
         let fileName = `matthew_holland_resume.pdf`;
         let filePath = path.join(__dirname, `../public/docs/${fileName}`);
-
         let file = fs.createReadStream(filePath);
         let stat = fs.statSync(filePath);
+
+        // Open the pdf in the browser pdf tab
         res.setHeader('Content-Length', stat.size);
         res.setHeader('Content-Type', 'application/pdf');
         res.setHeader('Content-Disposition', 'inline; filename=quote.pdf');
