be25d5fcc5fea4fa80f1d5332bd6fb1e0ff33fef
Matt Holland <mtholland10@gmail.com>
2017-01-23 00:33:43 -0600

    Test routes

--- controllers/documentController.js
+++ controllers/documentController.js
@@ -4,8 +4,9 @@ const path = require('path');
 
 module.exports = {
     downloadResume: (req, res) => {
-        let file = path.join(__dirname, '../public/docs/matthew_holland_resume.pdf');
-        console.log('file: ' + file)
+        let fileName = 'matthew_holland_resume.pdf';
+        let file = path.join(__dirname, `../public/docs/${fileName}`);
+
         res.download(file, function (err) {
             if (err) {
                 console.log('Error: ' + err);
