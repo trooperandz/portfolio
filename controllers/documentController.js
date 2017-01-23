ab6ac2f0b7f506595d090e4e404aa9696ec1fe9a
Matt Holland <mtholland10@gmail.com>
2017-01-23 16:05:46 -0600

    Update doc download script to open with inline disposition

--- controllers/documentController.js
+++ controllers/documentController.js
@@ -1,18 +1,26 @@
 'use strict';
 
-const path = require('path');
+const path = require('path'),
+      fs = require('fs');
 
 module.exports = {
     downloadResume: (req, res) => {
-        let fileName = 'matthew_holland_resume.pdf';
-        let file = path.join(__dirname, `../public/docs/${fileName}`);
+        let fileName = `matthew_holland_resume.pdf`;
+        let filePath = path.join(__dirname, `../public/docs/${fileName}`);
 
+        let file = fs.createReadStream(filePath);
+        let stat = fs.statSync(filePath);
+        res.setHeader('Content-Length', stat.size);
+        res.setHeader('Content-Type', 'application/pdf');
+        res.setHeader('Content-Disposition', 'inline; filename=quote.pdf');
+        file.pipe(res);
+        /* Download script
         res.download(file, function (err) {
             if (err) {
                 console.log('Error: ' + err);
             } else {
                 console.log('Success!');
             }
-        });
+        });*/
     },
 }
