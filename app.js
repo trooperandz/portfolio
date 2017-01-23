78239dbaca26bb3b37b101923b14c26b69fa2bfb
Matt Holland <mtholland10@gmail.com>
2017-01-23 00:42:34 -0600

    Fix routes

--- app.js
+++ app.js
@@ -25,9 +25,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/portfolio', indexRoute);
-app.use('/portfolio/contact', contactRoute);
-app.use('/portfolio/resume', documentRoute);
+app.use('/', indexRoute);
+app.use('/contact', contactRoute);
+app.use('/resume', documentRoute);
 // app.use('/users', users);
 
 // catch 404 and forward to error handler
